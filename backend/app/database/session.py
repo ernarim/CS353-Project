c31b8e3ff27a8213a13f98b78879686870a43756
ernarim <hayrettin.arim@ug.bilkent.edu.tr>
2024-05-01 15:51:17 +0300

    last commit

--- backend/app/database/session.py
+++ backend/app/database/session.py
@@ -1,9 +1,12 @@
 import psycopg2
 from fastapi import FastAPI
+import os
+from dotenv import dotenv_values
 
+backend_env_path = os.path.join(os.path.dirname(__file__), ".env")
+backend_env = dotenv_values(backend_env_path)
+DATABASE_URL = backend_env.get("DATABASE_URL")
 
-
-DATABASE_URL = "postgresql://postgres:admin123@localhost:5432/cs353"
 conn = psycopg2.connect(DATABASE_URL)
 cursor = conn.cursor()
 
--- backend/run.sh
+++ backend/run.sh
@@ -1,1 +1,2 @@
+. .venv/bin/activate
 RUN_TYPE=local uvicorn app.main:app --reload --host 0.0.0.0 --port 8000
